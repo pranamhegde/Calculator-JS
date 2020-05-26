76d76a5915910af56e1f516dac51906b65b0b82d
Pranam Hegde <pranamhegde3@gmail.com>
2020-05-26 23:46:51 +0530

    second commit

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 
 
+document.querySelector("input").value = 0;
 
-
-var numberOfButtons = document.querySelectorAll(".btn").length;
+var numberOfButtons = document.querySelectorAll(".number").length;
 
 for( var i = 0; i < numberOfButtons; i++){
-    document.querySelectorAll(".btn")[i].addEventListener("click", function() {
-        var display = document.getElementsByClassName("btn").value;
-         document.querySelector("input").innerHTML = display;
-      
-    });
+    document.querySelectorAll(".number")[i].addEventListener("click", function() {
+        // var display = document.getElementsByClassName("btn").value;
+        //  document.querySelector(".input").innerHTML = "45"; //Incorrect as ot updates only the string
+        document.querySelector("input").value +=  document.querySelector(".number").value;
+        
     
 }
 
-
+    function onClickAC () {
+        document.querySelector("input").value = 0;
+    }
