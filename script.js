1330ebf1a6e2b7daa4b24987e8b5311cb3261052
Pranam Hegde <pranamhegde3@gmail.com>
2020-05-27 00:09:01 +0530

    Second Commit

--- script.js
+++ script.js
@@ -8,11 +8,41 @@ for( var i = 0; i < numberOfButtons; i++){
     document.querySelectorAll(".number")[i].addEventListener("click", function() {
         // var display = document.getElementsByClassName("btn").value;
         //  document.querySelector(".input").innerHTML = "45"; //Incorrect as ot updates only the string
-        document.querySelector("input").value +=  document.querySelector(".number").value;
-        
+        var calcNumber = document.querySelector(".number").value;
+        document.querySelector(".input").value = parseInt(document.querySelector(".input").value);
+        document.querySelector(".input").value += parseInt(calcNumber);
+        alert(parseInt(document.querySelectorAll(".number")[0].value) + parseInt(document.querySelectorAll(".number")[1].value));
+    });
     
 }
 
     function onClickAC () {
         document.querySelector("input").value = 0;
     }
+
+    document.querySelectorAll(".operator").addEventListener("click", function(){
+        var operate;
+
+        switch (operate) {
+            case '+':
+                document.querySelector(".input") += document.querySelector(".number").value;
+                break;
+
+            case '-':
+                document.querySelector(".input") -= document.querySelector(".number").value;
+                break;
+
+            case '*':
+                document.querySelector(".input") *= document.querySelector(".number").value;
+                break;
+
+            case '/':
+                document.querySelector(".input") /= document.querySelector(".number").value;
+                break;
+            
+        
+            default:
+                console.log("Default");
+                break;
+        }
+    });
